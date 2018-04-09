ae5fed0bd9f332903f3e81e9a17e390a2a7871f4
Paul Waddell <paul.waddell@gmail.com>
2018-04-08 20:59:03 -0700

    New tenure choice notebook using updated synthesized households

--- scripts/datasources.py
+++ scripts/datasources.py
@@ -4,13 +4,14 @@ import zipfile
 
 # path relative to where python is run from
 z = zipfile.ZipFile('data/bay_area_test_data_20180302.zip')
+s = zipfile.ZipFile('data/sfbay_synth_pop.zip')
 
 # reading data directly from zipfiles is nice, but it doesn't work with symlinks (on my 
 # machine at least) - is there a workaround?
 
 @orca.table(cache=True)
 def households():
-    df = pd.read_csv(z.open('bay_area_test_data/households.csv'))
+    df = pd.read_csv('data/sfbay_synth_pop/sfbay_households_2018_04_08.csv')
     return df
 
 @orca.table(cache=True)
@@ -19,4 +20,4 @@ def buildings():
     return df
 
 # specify key relationships
-orca.broadcast('buildings', 'households', cast_index=True, onto_on='building_id')
+#orca.broadcast('buildings', 'households', cast_index=True, onto_on='building_id')
